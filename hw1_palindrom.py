5f39d8afcaca6abe08aceab63470589cd71ebeb5
Egor <catsmeowmeoww@gmail.com>
2019-09-28 21:38:28 +0300

    version_1.2

---
+++ hw1_palindrom.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+def check_palindrom(input_string):
+    if input_string == input_string[::-1]:
+        return True
+    else:
+        return False
+    raise NotImplementedError
+
