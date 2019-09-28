8b13abe6d6948a4ebc27c7e7c4e6cd0c60e633a3
Egor <catsmeowmeoww@gmail.com>
2019-09-28 21:38:28 +0300

    1

---
+++ hw1_winversion.py
@@ -0,0 +1,11 @@
+# def word_inversion(input_lst):
+#     '''
+#     Метод инвертирующий порядок слов в строке inplace (без выделения доп памяти)
+#     :param input_lst: строка-массив букв (['H', 'i']). Пробелы одиночные
+#     :return: None Все изменения в input_lst проходят
+#     '''
+#
+#     raise NotImplementedError
+a = ['П', 'р', 'и', 'в', 'е', 'т', ' ', 'д', 'р', 'у', 'г']
+for i in a:
+    print(i)
